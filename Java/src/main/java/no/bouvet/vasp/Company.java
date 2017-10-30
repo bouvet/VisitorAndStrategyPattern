a95bbcb87af0ced60fba28df0899cb4ba117a34a
oysteinjakobsen <ojapublic@manglerud.net>
2017-10-30 09:21:18 +0100

    Endret slik at generateJsonReport og generateXmlReport er eksponert og kalles fra testene.

--- Java/src/main/java/no/bouvet/vasp/Company.java
+++ Java/src/main/java/no/bouvet/vasp/Company.java
@@ -6,9 +6,6 @@ import java.util.stream.Collectors;
 
 public class Company {
 
-	public static final String XML_FORMAT = "xml";
-	public static final String JSON_FORMAT = "json";
-
 	private List<Worker> workers;
 
 	private ReportStrategy reportStrategy;
@@ -21,12 +18,8 @@ public class Company {
 		workers.add(worker);
 	}
 
-	public void setReportFormat(String reportFormat) {
-		if (JSON_FORMAT.equals(reportFormat)) {
-			reportStrategy = new JsonReport();
-		} else {
-			reportStrategy = new XmlReport();
-		}
+	public void setReportStrategy(ReportStrategy reportStrategy) {
+		this.reportStrategy = reportStrategy;
 	}
 
 	public String generateReport() {
